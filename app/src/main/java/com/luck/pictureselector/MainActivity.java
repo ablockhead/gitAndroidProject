727f7d30160274718fecd800287cefc0b244e746
shuachi_xuliang <1498470355@qq.com>
2019-07-22 18:19:01 +0800

    '暂存代码'

--- app/src/main/java/com/luck/pictureselector/MainActivity.java
+++ app/src/main/java/com/luck/pictureselector/MainActivity.java
@@ -1,126 +1,126 @@
-package com.luck.pictureselector;
-
-import android.Manifest;
-import android.app.Activity;
-import android.arch.lifecycle.Observer;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.support.annotation.IdRes;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.GridLayout;
-import android.widget.ImageView;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.luck.picture.lib.PictureSelector;
-import com.luck.picture.lib.config.PictureConfig;
-import com.luck.picture.lib.config.PictureMimeType;
-import com.luck.picture.lib.entity.LocalMedia;
-import com.luck.picture.lib.permissions.RxPermissions;
-import com.luck.picture.lib.tools.PictureFileUtils;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
-    private List<LocalMedia> selectionMedia;
-    private Activity activity;
-    private GridLayout gridLayout;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        gridLayout = findViewById(R.id.gridlayout_post);
-    }
-
-    public void chooseImage(View view){
-        System.out.println("'12456'");
-//         进入相册 以下是例子：不需要的api可以不写
-        PictureSelector.create(MainActivity.this)
-                .openGallery(PictureMimeType.ofImage())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
-                .theme(R.style.picture_shuchi_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
-                .maxSelectNum(8)// 最大图片选择数量
-                .minSelectNum(1)// 最小选择数量
-                .imageSpanCount(3)// 每行显示个数
-                .selectionMode(PictureConfig.MULTIPLE )// 多选 or 单选
-                .previewImage(true)// 是否可预览图片
-                .previewVideo(false)// 是否可预览视频
-                .enablePreviewAudio(false) // 是否可播放音频
-                .isCamera(true)// 是否显示拍照按钮
-                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
-                //.imageFormat(PictureMimeType.PNG)// 拍照保存图片格式后缀,默认jpeg
-                .setOutputCameraPath("/CustomPath")// 自定义拍照保存路径/
-                .enableCrop(true)// 是否裁剪
-                .compress(true)// 是否压缩
-                .synOrAsy(true)//同步true或异步false 压缩 默认同步
-                //.compressSavePath(getPath())//压缩图片保存地址
-                //.sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
-                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
-                .withAspectRatio(1,1)// 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
-                .hideBottomControls(true)// 是否显示uCrop工具栏，默认不显示
-                .isGif(false)// 是否显示gif图片
-                .freeStyleCropEnabled(false)// 裁剪框是否可拖拽
-                .circleDimmedLayer(false)// 是否圆形裁剪
-                .showCropFrame(true)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
-                .showCropGrid(true)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
-                .openClickSound(false)// 是否开启点击声音
-                .selectionMedia(selectionMedia)// 是否传入已选图片
-                //.isDragFrame(false)// 是否可拖动裁剪框(固定)
-//                        .videoMaxSecond(15)
-//                        .videoMinSecond(10)
-                //.previewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
-                //.cropCompressQuality(90)// 裁剪压缩质量 默认100
-                .minimumCompressSize(100)// 小于100kb的图片不压缩
-                //.cropWH()// 裁剪宽高比，设置如果大于图片本身宽高则无效
-                .rotateEnabled(false) // 裁剪是否可旋转图片
-                //.scaleEnabled(true)// 裁剪是否可放大缩小图片
-                //.videoQuality()// 视频录制质量 0 or 1
-                //.videoSecond()//显示多少秒以内的视频or音频也可适用
-                //.recordVideoSecond()//录制视频秒数 默认60s
-                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case PictureConfig.CHOOSE_REQUEST:
-                    // 图片、视频、音频选择结果回调
-                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
-                    // 例如 LocalMedia 里面返回三种path
-                    // 1.media.getPath(); 为原图path
-                    // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true  注意：音视频除外
-                    // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true  注意：音视频除外
-                    // 如果裁剪并压缩了，以取压缩路径为准，因为是先裁剪后压缩的
-                    addGroupImage(selectList);
-                    break;
-            }
-        }
-    }
-
-    /**
-     * 显示图片在Activity中
-     */
-    private void addGroupImage(List<LocalMedia> list){
-        for(LocalMedia localMedia : list){
-            ImageView imageView = new ImageView(this);
-            imageView.setLayoutParams(new RadioGroup.LayoutParams(250,250));
-            imageView.setPadding(25,25,25,25);
-            imageView.setImageURI(Uri.fromFile(new File(localMedia.getCutPath())));
-            gridLayout.addView(imageView);
-        }
-    }
-}
+//package com.luck.pictureselector;
+//
+//import android.Manifest;
+//import android.app.Activity;
+//import android.arch.lifecycle.Observer;
+//import android.content.Intent;
+//import android.net.Uri;
+//import android.os.Bundle;
+//import android.os.Environment;
+//import android.support.annotation.IdRes;
+//import android.support.annotation.Nullable;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.GridLayoutManager;
+//import android.support.v7.widget.RecyclerView;
+//import android.util.Log;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.CheckBox;
+//import android.widget.CompoundButton;
+//import android.widget.GridLayout;
+//import android.widget.ImageView;
+//import android.widget.RadioGroup;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import com.luck.picture.lib.PictureSelector;
+//import com.luck.picture.lib.config.PictureConfig;
+//import com.luck.picture.lib.config.PictureMimeType;
+//import com.luck.picture.lib.entity.LocalMedia;
+//import com.luck.picture.lib.permissions.RxPermissions;
+//import com.luck.picture.lib.tools.PictureFileUtils;
+//
+//import java.io.File;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class MainActivity extends AppCompatActivity {
+//    private List<LocalMedia> selectionMedia;
+//    private Activity activity;
+//    private GridLayout gridLayout;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        gridLayout = findViewById(R.id.gridlayout_post);
+//    }
+//
+//    public void chooseImage(View view){
+//        System.out.println("'12456'");
+////         进入相册 以下是例子：不需要的api可以不写
+//        PictureSelector.create(MainActivity.this)
+//                .openGallery(PictureMimeType.ofImage())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+//                .theme(R.style.picture_shuchi_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
+//                .maxSelectNum(8)// 最大图片选择数量
+//                .minSelectNum(1)// 最小选择数量
+//                .imageSpanCount(3)// 每行显示个数
+//                .selectionMode(PictureConfig.MULTIPLE )// 多选 or 单选
+//                .previewImage(true)// 是否可预览图片
+//                .previewVideo(false)// 是否可预览视频
+//                .enablePreviewAudio(false) // 是否可播放音频
+//                .isCamera(true)// 是否显示拍照按钮
+//                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+//                //.imageFormat(PictureMimeType.PNG)// 拍照保存图片格式后缀,默认jpeg
+//                .setOutputCameraPath("/CustomPath")// 自定义拍照保存路径/
+//                .enableCrop(true)// 是否裁剪
+//                .compress(true)// 是否压缩
+//                .synOrAsy(true)//同步true或异步false 压缩 默认同步
+//                //.compressSavePath(getPath())//压缩图片保存地址
+//                //.sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+//                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+//                .withAspectRatio(1,1)// 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
+//                .hideBottomControls(true)// 是否显示uCrop工具栏，默认不显示
+//                .isGif(false)// 是否显示gif图片
+//                .freeStyleCropEnabled(false)// 裁剪框是否可拖拽
+//                .circleDimmedLayer(false)// 是否圆形裁剪
+//                .showCropFrame(true)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
+//                .showCropGrid(true)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
+//                .openClickSound(false)// 是否开启点击声音
+//                .selectionMedia(selectionMedia)// 是否传入已选图片
+//                //.isDragFrame(false)// 是否可拖动裁剪框(固定)
+////                        .videoMaxSecond(15)
+////                        .videoMinSecond(10)
+//                //.previewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
+//                //.cropCompressQuality(90)// 裁剪压缩质量 默认100
+//                .minimumCompressSize(100)// 小于100kb的图片不压缩
+//                //.cropWH()// 裁剪宽高比，设置如果大于图片本身宽高则无效
+//                .rotateEnabled(false) // 裁剪是否可旋转图片
+//                //.scaleEnabled(true)// 裁剪是否可放大缩小图片
+//                //.videoQuality()// 视频录制质量 0 or 1
+//                //.videoSecond()//显示多少秒以内的视频or音频也可适用
+//                //.recordVideoSecond()//录制视频秒数 默认60s
+//                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == RESULT_OK) {
+//            switch (requestCode) {
+//                case PictureConfig.CHOOSE_REQUEST:
+//                    // 图片、视频、音频选择结果回调
+//                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+//                    // 例如 LocalMedia 里面返回三种path
+//                    // 1.media.getPath(); 为原图path
+//                    // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true  注意：音视频除外
+//                    // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true  注意：音视频除外
+//                    // 如果裁剪并压缩了，以取压缩路径为准，因为是先裁剪后压缩的
+//                    addGroupImage(selectList);
+//                    break;
+//            }
+//        }
+//    }
+//
+//    /**
+//     * 显示图片在Activity中
+//     */
+//    private void addGroupImage(List<LocalMedia> list){
+//        for(LocalMedia localMedia : list){
+//            ImageView imageView = new ImageView(this);
+//            imageView.setLayoutParams(new RadioGroup.LayoutParams(250,250));
+//            imageView.setPadding(25,25,25,25);
+//            imageView.setImageURI(Uri.fromFile(new File(localMedia.getCutPath())));
+//            gridLayout.addView(imageView);
+//        }
+//    }
+//}
